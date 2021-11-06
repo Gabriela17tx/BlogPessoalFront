72c26e05a7b23b95343bb8fd24e968a9763e519a
gabriela <gabriela17teixeira@gmail.com>
2021-11-05 21:02:56 -0300

    Rotas de entrar e cadastrar

---
+++ src/app/model/Postagem.ts
@@ -0,0 +1,18 @@
+import { Tema } from "./Tema";
+import { Usuario } from "./Usuario";
+
+export class Postagem{
+
+  public date: Date;
+  public id: number;
+  public tema: Tema;
+  public texto: string;
+  public titulo: string;
+  public usuario: Usuario;
+
+
+
+
+
+
+}
