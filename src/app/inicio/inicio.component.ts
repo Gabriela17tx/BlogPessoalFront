762376705ef8eca67bad3b5bd4139c5b37c80425
gabriela <gabriela17teixeira@gmail.com>
2021-11-11 14:33:47 -0300

    Implementação do put e delete de tema!

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,13 @@
+import { identifierName } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,16 +16,45 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
-  private router: Router
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  postagem: Postagem = new Postagem()
+  idUsuario = environment.id
+  usuario: Usuario = new Usuario()
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService,
+    private authh: AuthService
+  ) {}
+
+
+  ngOnInit() {
 
     if(environment.token == ''){
-      alert ('Sua seção expirou, faça o login novamente')
-      this.router.navigate(['/entrar'])
+      alert ('')
+      this.router.navigate(['/entrar']);
     }
+    this.getAllTemas();
+  }
 
+  getAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=>{
+      this.listaTemas = resp
+    })
+  }
+  getTemaById(){
+    this.temaService.getTemaById(this.idTema).subscribe((resp: Tema)=>{
+      this.tema = resp
+    })
+  }
+  publicar(){
+    this.tema.idTena = this.idTema = this.idTema
+    this.postagem.temaRelacionado = this.tema
   }
 
 }
+
+tema.idTema = this.idTema
+    this.postagem.temaRelacionado = this.tema
